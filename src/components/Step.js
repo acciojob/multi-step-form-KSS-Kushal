6a252fe2758ab40576cbff9a60249e2435650c5b
Kushal Ghosh <kushalghosh075@gmail.com>
2024-05-29 21:31:57 +0530

    done

--- src/components/Step.js
+++ src/components/Step.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Step = ({step, setStep, name, id1, id2, label1, label2, value1, value2, setValue1, setValue2}) => {
+    console.log(step)
   return (
     <div id={`step${step}`}>
         <p>{name}</p>
@@ -12,9 +13,9 @@ const Step = ({step, setStep, name, id1, id2, label1, label2, value1, value2, se
             <label htmlFor={id2}>{label2}</label>
             <input type="text" id={id2} className="" value={value2} onChange={e=>setValue2(e.target.value)} />
         </div>
-        {step!==1 && <button onClick={()=>setStep(prev=>prev-1)}>Previous</button>}
-        {step!==3 && <button onClick={()=>setStep(prev=>prev+1)}>Next</button>}
-        {step===3 && <button>Submit</button>}
+        {step!==1 && <button id='prev-btn' onClick={()=>setStep(prev=>prev-1)}>Previous</button>}
+        {step!==3 && <button id='next-btn' onClick={()=>setStep(prev=>prev+1)}>Next</button>}
+        {step===3 && <button id='submit-btn'>Submit</button>}
     </div>
   )
 }
