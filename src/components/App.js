1941a1ad69db18ef50a61b28d5b4661ffdc04f2f
Kushal Ghosh <kushalghosh075@gmail.com>
2024-05-29 21:34:31 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,9 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        {step===1 && <Step step={step} setStep={setStep} name={"Customer Details"} id1={"first_name"} id2={"last_name"} label1={"First Name"} label2={"Last Name"} />}
-        {step===2 && <Step step={step} setStep={setStep} name={"Car Details"} id1={"model"} id2={"car_price"} label1={"Brand"} label2={"Model"} />}
-        {step===3 && <Step step={step} setStep={setStep} name={"Payment Details"} id1={"card_info"} id2={"expiry_date"} label1={"Credit Card Number"} label2={"Expiration Date"} />}
+        <Step step={step} setStep={setStep} name={"Customer Details"} id1={"first_name"} id2={"last_name"} label1={"First Name"} label2={"Last Name"} />
+        <Step step={step} setStep={setStep} name={"Car Details"} id1={"model"} id2={"car_price"} label1={"Brand"} label2={"Model"} />
+        <Step step={step} setStep={setStep} name={"Payment Details"} id1={"card_info"} id2={"expiry_date"} label1={"Credit Card Number"} label2={"Expiration Date"} />
     </div>
   )
 }
